88d29d1794c99db7e57ca949faa4182dfd7d3796
T.C. Hollingsworth <tchollingsworth@gmail.com>
2013-03-31 16:17:00 -0700

    bump to 0.10.3
    now uses the official tarballs from libuv.org that were recently
    introduced upstream

--- libuv-snapshot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/joyent/libuv.git
-pushd libuv
-git archive --format=tar --prefix=libuv-0.10.0/ 5462dab8890c414154690d8a45b1384301b27436 | gzip -nc > ../libuv-0.10.0git5462dab.tar.gz
-popd
-
-rm -Rf libuv
